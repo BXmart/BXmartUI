8b15a05dad8141e5bc9b5da7089cd52a5cdeee71
salegrebxmart <salegre@bexmarter.com>
2024-05-17 14:22:08 +0200

    Set Tailwind

---
+++ src/components/atoms/index.ts
@@ -0,0 +1,3 @@
+export * from './Button';
+export * from './Icons/IconLoading';
+export * from './Icons';
---
+++ src/components/index.ts
@@ -0,0 +1,3 @@
+export * from './atoms/Button';
+export * from './atoms/Icons/IconLoading';
+export * from './atoms/Icons';
