b2a6e4301407fdf993211b20b8f9d12458282dfc
Francisco Roldán Moreno <froldan@bexmarter.com>
2024-05-24 02:49:27 +0200

    Update on StoryBook

---
+++ .storybook/bxmart.ts
@@ -0,0 +1,8 @@
+import { create } from '@storybook/theming/create';
+
+export default create({
+  base: 'light',
+  brandTitle: `<img src="/logo.png" width="200px" height="30px" alt="BXmart UI" />`,
+  brandUrl: 'https://bexmarter.com',
+  brandTarget: '_self',
+});
---
+++ .storybook/manager.ts
@@ -0,0 +1,25 @@
+import { addons } from '@storybook/manager-api';
+import bxmartTheme from './bxmart';
+
+addons.setConfig({
+  navSize: 300,
+  bottomPanelHeight: 300,
+  rightPanelWidth: 300,
+  panelPosition: 'bottom',
+  enableShortcuts: true,
+  showToolbar: true,
+  theme: bxmartTheme,
+  selectedPanel: undefined,
+  initialActive: 'sidebar',
+  sidebar: {
+    showRoots: false,
+    collapsedRoots: ['other'],
+  },
+  toolbar: {
+    title: { hidden: true },
+    zoom: { hidden: true },
+    eject: { hidden: true },
+    copy: { hidden: true },
+    fullscreen: { hidden: true },
+  },
+});
